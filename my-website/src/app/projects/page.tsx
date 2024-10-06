bc70233ffb0224abf657a33c98d21e1243834b0f
DylanFitzpatrick01 <dylanfitzpatrick33@gmail.com>
2024-10-06 20:18:13 +0100

    added space invaders demo video

--- my-website/src/app/projects/page.tsx
+++ my-website/src/app/projects/page.tsx
@@ -27,60 +27,65 @@ export default function Page() {
                     </a>
                 </p>
                 <h2 className="flex text-2xl pt-6 pb-2 items-center justify-center font-bold">Image Analysis and Rating</h2>
-                <p className="px-20 pb-10 break-words"> A project as part of the module Software Engineering Project I in the 2nd year of my degree. This was a group project 
-                        comprised of 6 members including myself. This project allows a user to upload an image to a website in order to rate 
-                        an image based on various metrics which our team chose, using NextJS and NodeJS. The metrics used were: brisque score, 
-                        tags, trend search i.e. the image&apos;s relevance, reverse image search to show how many similar images exist and the type 
-                        of the image e.g. clip art, photograph etc. The backend was written using ReactJS. My main contribution to the project 
-                        was implementing the reverse image search metric.
+                <p className="px-20 pb-10 break-words"> A project as part of the module Software Engineering Project I in the 2nd year of my degree. This was a group project
+                    comprised of 6 members including myself. This project allows a user to upload an image to a website in order to rate
+                    an image based on various metrics which our team chose, using NextJS and NodeJS. The metrics used were: brisque score,
+                    tags, trend search i.e. the image&apos;s relevance, reverse image search to show how many similar images exist and the type
+                    of the image e.g. clip art, photograph etc. The backend was written using ReactJS. My main contribution to the project
+                    was implementing the reverse image search metric.
                     <a href="https://github.com/AyoKassim/Shutterstock" target="_blank" className="hover:text-gray-500 flex text-emerald-400 pt-2">
                         Github Repository
                     </a>
                 </p>
                 <h2 className="flex text-2xl pt-6 pb-2 items-center justify-center font-bold">GitHub Statistics</h2>
                 <p className="px-20 pb-10 break-words">A web based project which takes data from Microsoft&apos;s open source calculator. Shows the user various graphs
-                        displaying contributors statistic e.g. most issues opened/closed. Also allows the user to search for a specific
-                        contributor. My contribution to this project was implementing a MySQL database to store the data.
+                    displaying contributors statistic e.g. most issues opened/closed. Also allows the user to search for a specific
+                    contributor. My contribution to this project was implementing a MySQL database to store the data.
                     <a href="https://github.com/eimearryan/CSU33012-Main-Project" target="_blank" className="hover:text-gray-500 flex text-emerald-400 pt-2">
                         Github Repository
                     </a>
                 </p>
                 <h2 className="flex text-2xl pt-6 pb-2 items-center justify-center font-bold">Vancouver Bus Management System</h2>
                 <p className="px-20 pb-10 break-words"> A Java based program which implements Dijkstra&apos;s algorithm. This program allows to user to find the shortest
-                        route between two bus stops, search stop details by the stop&apos;s name and search for a stop based on the arrival time.
-                        The data is taken from 3 .txt files.
+                    route between two bus stops, search stop details by the stop&apos;s name and search for a stop based on the arrival time.
+                    The data is taken from 3 .txt files.
                     <a href="https://github.com/DylanFitzpatrick01/Bus-Management-System" target="_blank" className="hover:text-gray-500 flex text-emerald-400 pt-2">
                         Github Repository
                     </a>
                 </p>
                 <h2 className="flex text-2xl pt-6 pb-2 items-center justify-center font-bold">Space Invaders</h2>
                 <p className="px-20 pb-10 break-words"> A solo project made in Processing 3 which allows the user to play a game akin to Space Invaders.
+                    <p className="flex text-2xl pt-6 pb-2 font-bold">Demo</p>
+                    <video className="h-full w-full rounded-lg" controls>
+                        <source src="/spaceinvadersdemo.mp4" type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
                     <a href="https://github.com/DylanFitzpatrick01/AlienHunterGame" target="_blank" className="hover:text-gray-500 flex text-emerald-400 pt-2">
                         Github Repository
                     </a>
                 </p>
                 <h2 className="flex text-2xl pt-6 pb-2 items-center justify-center font-bold">Web Calculator</h2>
                 <p className="px-20 pb-10 break-words"> A group project as part of the Software Engineering module in 3rd year. The goal was to create a functioning calculator
-                        and deploy it as a web app. My contribution to the project was connecting the frontend to the backend which I learned a lot 
-                        from.
+                    and deploy it as a web app. My contribution to the project was connecting the frontend to the backend which I learned a lot
+                    from.
                     <a href="https://github.com/MylanaBulat/webCalculator" target="_blank" className="hover:text-gray-500 flex text-emerald-400 pt-2">
                         Github Repository
                     </a>
                 </p>
                 <h2 className="flex text-2xl pt-6 pb-2 items-center justify-center font-bold">File Retrieval Protocol</h2>
-                <p className="px-20 pb-10 break-words"> The first assignment in my Computer Networks module in 3rd year. The aim of this assignment was to provide a means for one or more 
-                        clients to retrieve files from one or more workers based on User Datagram Protocol (UDP) packets. This is done through the use of an 
-                        ingress node which communicates with both the client and worker nodes.
+                <p className="px-20 pb-10 break-words"> The first assignment in my Computer Networks module in 3rd year. The aim of this assignment was to provide a means for one or more
+                    clients to retrieve files from one or more workers based on User Datagram Protocol (UDP) packets. This is done through the use of an
+                    ingress node which communicates with both the client and worker nodes.
                     <a href="https://github.com/DylanFitzpatrick01/File-Retrieval-Protocol" target="_blank" className="hover:text-gray-500 flex text-emerald-400 pt-2">
                         Github Repository
                     </a>
                 </p>
                 <h2 className="flex text-2xl pt-6 pb-2 items-center justify-center font-bold">Flow Forwarding</h2>
-                <p className="px-20 pb-10 break-words"> The second assignment in my Computer Networks module in 3rd year. The aim of this assignment was to implement forward flows of packets. 
-                        Communication is achieved between an ”employee” and a ”deployment server” by including the destination address in the packet header. 
-                        The packet is then sent sent through a series of ”forwarders” which consult a ”controller” to determine where the next hop is. I put my
-                        own personal twist on it by allowing the user to request an appointment time and would be returned whether or not that appointment is 
-                        available.
+                <p className="px-20 pb-10 break-words"> The second assignment in my Computer Networks module in 3rd year. The aim of this assignment was to implement forward flows of packets.
+                    Communication is achieved between an ”employee” and a ”deployment server” by including the destination address in the packet header.
+                    The packet is then sent sent through a series of ”forwarders” which consult a ”controller” to determine where the next hop is. I put my
+                    own personal twist on it by allowing the user to request an appointment time and would be returned whether or not that appointment is
+                    available.
                     <a href="https://github.com/DylanFitzpatrick01/Flow-Forwarding" target="_blank" className="hover:text-gray-500 flex text-emerald-400 pt-2">
                         Github Repository
                     </a>
